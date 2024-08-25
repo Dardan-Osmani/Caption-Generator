3533478c6ccb0f5b792e608d5bdc17de875bf86c
do29239 <do29239@seeu.edu.mk>
2024-08-26 00:43:59 +0200

    Whole project

---
+++ src/components/Section.js
@@ -0,0 +1,29 @@
+import Logo from "@/components/Logo";
+
+export default function Section() {
+  return (
+    <section className="flex justify-around mt-8 sm:mt-12 items-center">
+      <div className="hidden sm:block bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
+        <video
+          src="https://dawid-epic-captions.s3.us-east-1.amazonaws.com/without-captions.mp4"
+          preload="auto"
+          muted
+          autoPlay
+          loop
+        ></video>
+      </div>
+      <div className="hidden sm:block">
+        <Logo />
+      </div>
+      <div className="bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
+        <video
+          src="https://dawid-epic-captions.s3.us-east-1.amazonaws.com/with-captions.mp4"
+          preload="auto"
+          muted
+          autoPlay
+          loop
+        ></video>
+      </div>
+    </section>
+  );
+}
